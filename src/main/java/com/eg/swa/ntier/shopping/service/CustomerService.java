15746fbd49a99f6a02473d084fd4244307178b87
MoZyadaCS <mostafazyada@protonmail.com>
2024-03-11 19:03:17 +0200

    add create customer method in customer service

--- src/main/java/com/eg/swa/ntier/shopping/service/CustomerService.java
+++ src/main/java/com/eg/swa/ntier/shopping/service/CustomerService.java
@@ -2,6 +2,8 @@ package com.eg.swa.ntier.shopping.service;
 
 import java.util.Optional;
 
+import com.eg.swa.ntier.shopping.dto.CustomerDto;
+import com.eg.swa.ntier.shopping.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,26 @@ public class CustomerService {
 	
 	@Autowired
 	private CustomerRepository customerRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	public Optional<Customer> getCustomerById(Long customerId) {
 		return customerRepository.findById(customerId);
+	}
+
+	public void CreateCustomer(CustomerDto customerDto){
+		Optional<Customer> optionalCustomer = customerRepository.findByEmail(customerDto.getEmail());
+		if(optionalCustomer.isPresent()){
+			throw new RuntimeException("Customer Has Account Associated With This Email");
+		}
+		Customer customer = new Customer();
+		customer.setAddress(customerDto.getAddress());
+		customer.setName(customerDto.getName());
+		customer.setEmail(customerDto.getEmail());
+		customer.setPassword(customerDto.getPassword());
+		customer.setRole(roleRepository.findByName("CUSTOMER").get());
+		customerRepository.save(customer);
 	}
 
 }
